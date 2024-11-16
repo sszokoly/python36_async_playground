cc2d0244349ff7a207c7d58f1072eccce7cf7f43
sszokoly <sszokoly@protonmail.com>
2024-11-15 18:15:38 -0700

    added indexable mapping

--- storage.py
+++ storage.py
@@ -1,30 +1,237 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from bisect import insort_left
 from collections import deque
-from abc import ABC, abstractmethod
-from collections.abc import Sequence
+from collections.abc import MutableMapping
+from itertools import islice
+from typing import Iterator, Optional, Tuple, Union, Any, Dict, List
 
-class AbstractStorage(ABC, Sequence):
-    @abstractmethod
-    def clear(self):
-        """Clears storage"""   
-    @abstractmethod
-    def append(self, x):
-        """Add x to the right side"""
-   
-class Storage(AbstractStorage):
-    def __init__(self, maxlen=None):
-        self._items = deque([], maxlen)
-    def append(self, x):
-        self._items.append(x)
-    def clear(self):
-        self._items.clear()
-    def __getitem__(self, i):
-        return self._items[i]
-    def __len__(self):
+class MemoryStorage(MutableMapping):
+    """
+    A mutable mapping that stores items in memory and supports a maximum
+    length, discarding the oldest items if the maximum length is exceeded.
+    Items are always returned ordered by keys. A key can also be integer or slice.
+    """
+    def __init__(self, items: Optional[Dict] = None, maxlen: Optional[int] = None) -> None:
+        """
+        Initialize the MemoryStorage object.
+
+        Parameters
+        ----------
+        items : dict, optional
+            Items to initialize the MemoryStorage with.
+        maxlen : int, optional
+            Maximum length of the MemoryStorage. If exceeded, oldest items are
+            discarded. If not provided, the MemoryStorage has no maximum length.
+        """
+        self._items = dict(items) if items else dict()
+        self._keys = deque(sorted(self._items.keys()) if items else [])
+        self.maxlen = maxlen
+    def __len__(self) -> int:
+        """Return the number of items stored in the MemoryStorage."""
         return len(self._items)
-    def __str__(self):
-        return self._items.__repr__()
 
-storage = Storage(3)
-storage.append({'id': '00001', 'local_ip': '10.10.10.48.58'})
-storage.append({'id': '00001', 'local_ip': '10.44.244.48.58'})
-print(storage)
+    def __iter__(self) -> Iterator[Any]:
+        """Return an iterator over the keys of the MemoryStorage."""
+        yield from self._keys
+
+    def __getitem__(self, key: Union[int, slice, Any]) -> Union[Any, List[Any]]:
+        """
+        Retrieve item(s) from the MemoryStorage using a key or slice.
+
+        Parameters
+        ----------
+        key : int | slice | Any
+            If an integer, returns the item at that index in the ordered keys.
+            If a slice, returns a list of items corresponding to the slice.
+            Otherwise, retrieves the item associated with the key.
+
+        Returns
+        -------
+        The item associated with the key or a list of items if the key is a slice.
+
+        Raises
+        ------
+        KeyError
+            If the key is an integer and out of bounds, or if the key does not exist.
+        """
+        if isinstance(key, slice):
+            if key.start is None:
+                start = 0
+            elif key.start < 0:
+                start = len(self._items) + key.start
+            else:
+                start = key.start
+            if key.stop is None:
+                stop = len(self._items)
+            elif key.stop < 0:
+                stop = len(self._items) + key.stop
+            else:
+                stop = key.stop
+            return [self._items[key] for key in
+                islice(self._keys, start, stop, key.step)]
+        if isinstance(key, int):
+            if self._items and len(self._items) > key:
+                return self._items[self._keys[key]]
+            else: 
+                raise KeyError
+        return self._items[key]
+
+    def get(self, key: Any, default: Any = None) -> Any:
+        """
+        Return the item associated with the key if it exists, otherwise return
+        the default if given.
+
+        Parameters
+        ----------
+        key : Any
+            The key of the item to retrieve.
+        default : Any, optional
+            The default value to return if the key does not exist.
+
+        Returns
+        -------
+        The item associated with the key, or the default if the key does not exist.
+        """
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __setitem__(self, key: Any, item: Any) -> None:
+        """
+        Add or update an item in the MemoryStorage.
+
+        If the key does not exist, it is added to the MemoryStorage. If the
+        MemoryStorage has a maximum length and the addition of this item would
+        exceed that length, the oldest key is discarded before adding the new
+        key.
+
+        Parameters
+        ----------
+        key : Any
+            The key to associate with the item.
+        item : Any
+            The item to store in the MemoryStorage.
+        """
+        if key in self._items:
+            self._items[key] = item
+        else:
+            if self.maxlen and len(self._items) == self.maxlen:
+                first_key = self._keys.popleft()
+                del self._items[first_key]
+            insort_left(self._keys, key)
+            self._items[key] = item
+
+    def __delitem__(self, key: Any) -> None:
+        """
+        Remove the item associated with the key from the MemoryStorage.
+
+        Parameters
+        ----------
+        key : Any
+            The key of the item to remove.
+
+        Raises
+        ------
+        KeyError
+            If the key does not exist in the MemoryStorage.
+        """
+        if key in self._items:
+            del self._items[key]
+            self._keys.remove(key)
+        else:
+            raise KeyError
+
+    def __contains__(self, key: Any) -> bool:
+        """
+        Check if the specified key exists in the MemoryStorage.
+
+        Parameters
+        ----------
+        key : Any
+            The key to check for existence in the MemoryStorage.
+
+        Returns
+        -------
+        bool
+            True if the key exists in the MemoryStorage, False otherwise.
+        """
+        return key in self._items
+
+    def keys(self) -> Iterator[Any]:
+        """
+        Generate an iterator over the keys in the MemoryStorage.
+
+        Returns
+        -------
+        Generator[Any]
+            An iterator over the keys in the order they were inserted.
+        """
+        return (key for key in self._keys)
+
+    def values(self) -> Iterator[Any]:
+        """
+        Generate an iterator over the values in the MemoryStorage.
+
+        Returns
+        -------
+        Generator[Any]
+            An iterator over the values in the order they were inserted.
+        """
+        return (self._items[key] for key in self._keys)
+
+    def items(self) -> Iterator[Tuple]:
+        """
+        Generate an iterator over the (key, value) pairs in the MemoryStorage.
+        The order of iteration is sorted by keys.
+
+        Returns
+        -------
+        Generator[Tuple]
+            An iterator over the (key, value) pairs in the MemoryStorage.
+        """
+        return ((key, self._items[key]) for key in self._keys)
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the MemoryStorage instance.
+
+        The string includes the name of the class, the items currently stored,
+        and the maximum length of the storage if applicable.
+        """
+        return f"{type(self).__name__}={self._items}, maxlen={self.maxlen})"
+
+    def __eq__(self, other) -> bool:
+        """
+        Check if two MemoryStorage instances are equal.
+
+        The comparison is done by checking if the items are equal and the maximum
+        length is the same. If the other object is not of the same class, it returns
+        NotImplemented.
+        """
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return list(self.items()) == list(other.items()) and self.maxlen == other.maxlen
+
+
+if __name__ == "__main__":
+    items = {('2004-10-20,11:09:11', '10.10.48.1', '00001'): {'id': '00001', 'end_time': '2004-10-20,11:10:11'}}
+    storage = MemoryStorage(items=items,maxlen=3)
+    storage2 = MemoryStorage(items=items,maxlen=3)
+    storage[('2004-10-20,11:09:10', '10.10.48.2', '00001')] = {'id': '00001', 'end_time': '2004-10-20,11:10:10'}
+    storage[('2004-10-20,11:09:13', '10.10.48.2', '00002')] = {'id': '00002', 'end_time': '2004-10-20,11:10:13'}
+    for k,v in storage.items():
+        print(k, v)
+    print(storage[0:2])
+    print(storage[-1])
+    print(storage.keys())
+    print(storage.values())
+    print(storage.items())
+    storage[('2004-10-20,11:09:12', '10.10.48.1', '00002')] = {'id': '00002', 'end_time': '2004-10-20,11:10:12'}
+    print(len(storage))
+    print(storage[-2:])
+    print(storage)
+    print(('2004-10-20,11:09:10', '10.10.48.2', '00001') in storage)
+    print(('2004-10-20,11:09:13', '10.10.48.2', '00002') in storage)
+    print(bool(storage == storage2))
