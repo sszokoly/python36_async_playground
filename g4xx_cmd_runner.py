8a37ee367025928585d9f235b5339d6803d7be00
sszokoly <sszokoly@protonmail.com>
2024-12-09 08:40:11 -0700

    update

--- g4xx_cmd_runner.py
+++ g4xx_cmd_runner.py
@@ -15,47 +15,60 @@ import sys
 from subprocess import Popen, PIPE
 from typing import Any, Coroutine, TypeVar, Optional, Set, Callable, Any, TypeVar, Tuple, List
 
-cmd_template = '''
+script_template = '''
 #!/usr/bin/expect
-log_user 0
+############################# Template Variables #############################
+
+set timeout {timeout}
+set host {host}
+set user {user}
+set passwd {passwd}
+set lastn_secs {lastn_secs}
+set rtp_stat {rtp_stat}
+set commands {{ {commands} }}
 set log_file {log_file}
+
+############################## Expect Variables ##############################
+
+set prompt "\\)# "
+array set commands_array {{}}
+array set rtp_sessions_array {{}}
+set global_ids [list]
+log_user 0
 
 if {{[info exists log_file] && $log_file ne "/dev/null"}} {{
     if {{[file exists $log_file]}} {{
         file delete $log_file
     }}
 }}
-
 exp_internal -f $log_file 0
-
-################################# Variables ##################################
-
-set timeout {timeout}
-set host {host}
-set user {user}
-set passwd {passwd}
-set prompt "\\)# "
-set commands {{ {commands} }}
-array set commands_array {{}}
 
 ################################# Procedures #################################
 
 proc to_json {{}} {{
-    global host gw_number commands_array
+    global host gw_number commands_array rtp_sessions_array
     set json "{{"
     append json "\\"host\\": \\"$host\\", "
     append json "\\"gw_number\\": \\"$gw_number\\", "  
-    if {{[llength [array names commands_array]] > 0}} {{
-        append json "\\"commands\\": {{"
-        foreach {{key value}} [array get commands_array] {{
-            append json "\\"$key\\": \\"$value\\", "
-        }}
-        set json [string trimright $json ", "]
-        append json "}}"
+    append json "\\"commands\\": {{"
+    foreach {{key value}} [array get commands_array] {{
+        append json "\\"$key\\": \\"$value\\", "
     }}
     set json [string trimright $json ", "]
-    append json "}}"
+    append json "}}, "
+    append json "\\"rtp_sessions\\": {{"
+    foreach {{key value}} [array get rtp_sessions_array] {{
+        append json "\\"$key\\": \\"$value\\", "
+    }}
+    set json [string trimright $json ", "]
+    append json "}}}}"
     return $json
+}}
+
+proc merge_lists {{list1 list2}} {{
+    set combined [concat $list1 $list2]
+    set result [lsort -unique $combined]
+    return $result
 }}
 
 proc clean_output {{output}} {{
@@ -104,7 +117,68 @@ proc cmd {{command}} {{
     return [string trimleft $result $command]
 }}
 
-#################################### Main ####################################
+proc get_active_global_ids {{}} {{
+    global host cmd parse_session_summary_line
+    set ids [list]
+    foreach line [split [cmd "show rtp-stat sessions active\\n"] "\\n"] {{
+        if {{[regexp {{^[0-9]+}} $line]}} {{
+            set result [parse_session_summary_line $line]
+            lassign $result session_id start_date start_time end_date end_time
+            lappend ids [format "%s,%s,%s,%s" $start_date $start_time $host $session_id]
+        }}
+    }}
+    return $ids
+}}
+
+proc get_recent_global_ids {{{{lastn_secs {{20}}}}}} {{
+    global host cmd parse_session_summary_line is_date1_gt_date2
+    set ref_datetime [exec date "+%Y-%m-%d,%H:%M:%S" -d "now - $lastn_secs secs"]
+    set ids [list]
+    foreach line [split [cmd "show rtp-stat sessions last 20\\n"] "\\n"] {{
+        if {{[regexp {{^[0-9]+}} $line]}} {{
+            set result [parse_session_summary_line $line]
+            lassign $result session_id start_date start_time end_date end_time
+            if {{$end_time ne "-"}} {{
+                set end_datetime [format "%s,%s" $end_date $end_time]
+                set is_end_datetime_gt_ref_datetime [is_date1_gt_date2 $end_datetime $ref_datetime]
+                if {{$is_end_datetime_gt_ref_datetime}} {{
+                    lappend ids [format "%s,%s,%s,%s" $start_date $start_time $host $session_id]
+                }}
+            }}
+        }}
+    }}
+    return $ids
+}}
+
+proc parse_session_summary_line {{input}} {{
+    set pattern {{^(\\S+)\\s+(\\S+),(\\S+)\\s+(\\S+)\\s+.*$}}
+    if {{[regexp $pattern $input _ id start_date start_time end_time]}} {{
+        # if end time rolled over to the next day
+        if {{$end_time < $start_time}} {{
+            set end_date [exec date "+%Y-%m-%d" -d "$start_date + 1 day"]
+        }} else {{
+            set end_date $start_date
+        }}
+        return [list $id "$start_date" "$start_time" "$end_date" "$end_time"]
+    }} else {{
+        puts stderr "Error: Input format does not match: $input";
+        return ""
+    }}
+}}
+
+proc is_date1_gt_date2 {{date1 date2}} {{
+    # Convert the date strings into epoch timestamps
+    set timestamp1 [clock scan $date1 -format "%Y-%m-%d,%H:%M:%S"]
+    set timestamp2 [clock scan $date2 -format "%Y-%m-%d,%H:%M:%S"]
+    # Compare the timestamps
+    if {{$timestamp1 > $timestamp2}} {{
+        return 1
+    }} else {{
+        return 0 
+    }}
+}}
+
+################################# Main Script ################################
 
 #Spawn SSH connection
 spawn ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $user@$host
@@ -145,6 +219,26 @@ foreach command $commands {{
     }}
 }}
 
+#Collect RTP statistics if requested
+if {{$rtp_stat}} {{
+    #Active global session ids
+    set active_global_ids [get_active_global_ids]
+    #Recent global session ids
+    set recent_global_ids [get_recent_global_ids $lastn_secs]
+    #Merged global session ids
+    set merged_global_ids [merge_lists $active_global_ids $recent_global_ids]
+
+    if {{$merged_global_ids ne {{}}}} {{
+        foreach global_id $merged_global_ids {{
+            lassign [split $global_id ","] start_date start_time host session_id
+            set output [cmd "show rtp-stat detailed $session_id\\n"]
+            if {{$output ne ""}} {{
+                set rtp_sessions_array($global_id) $output
+            }}
+        }}
+    }}
+}}
+
 send "exit\\n"
 
 #Output results in JSON format
@@ -163,7 +257,8 @@ def run_script(cmd):
 
 def main(args):
     kwargs = vars(args)
-    log_file = f"debug_{kwargs['host']}.log" if kwargs["d"] else "/dev/null"
+    kwargs['rtp_stat'] = int(kwargs['rtp_stat'])
+    log_file = f"debug_{kwargs['host']}.log" if kwargs["debug"] else "/dev/null"
     kwargs.update({"log_file": log_file})
     if kwargs["commands"]:
         cmds = []
@@ -171,7 +266,7 @@ def main(args):
             cmds.extend(item.split("|"))
         commands = " ".join(f'"{c}"' for c in cmds)
         kwargs["commands"] = commands
-    script = cmd_template.format(**kwargs)
+    script = script_template.format(**kwargs)
     cmd = f"/usr/bin/env expect -c '{script}'"
     stdout, stderr = run_script(cmd)
     if stdout:
@@ -186,7 +281,9 @@ if __name__ == "__main__":
     required.add_argument('-i', dest='host', type=str, required=True, help='G4xx IP address')
     required.add_argument('-u', dest='user', type=str, required=True, help='username')
     required.add_argument('-p', dest='passwd', type=str, required=True, help='password')
-    parser.add_argument('-d', action='store_true', default=False, help='enable debug logging')
+    parser.add_argument('-d', action='store_true', dest='debug', default=False, help='enable debug logging')
     parser.add_argument('-t', dest='timeout', default=10, help='timeout in secs')
-    parser.add_argument('commands', action='store', default='', nargs='+', help='G4xx commands')
+    parser.add_argument('-n', dest='lastn_secs', default=20, help='secs to look back in RTP statistics')
+    parser.add_argument('-r', dest='rtp_stat', action='store_true', default=False, help='collect RTP statistics')
+    parser.add_argument('commands', action='store', default='', nargs='*', help='G4xx commands')
     sys.exit(main(parser.parse_args()))
