b6ccd4dd53746efec69d99d91d4215d396a7a624
sszokoly <sszokoly@protonmail.com>
2025-01-11 12:11:15 -0700

    update

--- windows.py
+++ windows.py
@@ -5,6 +5,7 @@ import time
 import sys
 from abc import ABC, abstractmethod
 from bgw import BGW
+from itertools import islice
 from storage import SlicableOrderedDict
 
 bgws = [
@@ -14,11 +15,11 @@ bgws = [
 
 
 storage = SlicableOrderedDict(items={
-    "2025-01-10,10:51:36,001,00001": {
+    "2025-01-10,10:00:01,001,00001": {
         "gw_number": "001",
         "session_id": "00001",
-        "start_time": "2025-01-10,10:51:36",
-        "end_time": "10:52:48",
+        "start_time": "2025-01-10,10:00:01",
+        "end_time": "10:10:01",
         "local_addr": "10.10.48.1",
         "local_port": "2048",
         "remote_addr": "192.168.148.191",
@@ -26,38 +27,73 @@ storage = SlicableOrderedDict(items={
         "qos":  "ok",
         "codec": "G711U",
     },
-    "2025-01-10,10:53:36,001,00002": {
-        "gw_number": "001",
+    "2025-01-10,10:00:02,002,00002": {
+        "gw_number": "002",
         "session_id": "00001",
-        "start_time": "2025-01-10,10:53:36",
-        "end_time": "10:59:38",
-        "local_addr": "10.10.48.1",
+        "start_time": "2025-01-10,10:00:02",
+        "end_time": "10:10:02",
+        "local_addr": "10.10.48.2",
         "local_port": "2052",
-        "remote_addr": "10.10.48.2",
+        "remote_addr": "10.10.48.4",
         "remote_port": "2064",
         "qos":  "ok",
         "codec": "G711U",
     },
-    "2025-01-10,10:53:36,002,00012": {
-        "gw_number": "002",
+    "2025-01-10,10:00:03,003,00012": {
+        "gw_number": "003",
         "session_id": "00001",
-        "start_time": "2025-01-10,10:53:36",
-        "end_time": "10:59:38",
-        "local_addr": "10.10.48.2",
+        "start_time": "2025-01-10,10:00:03",
+        "end_time": "10:10:03",
+        "local_addr": "10.10.48.3",
         "local_port": "2064",
         "remote_addr": "10.10.48.1",
         "remote_port": "2052",
         "qos":  "Faulted",
         "codec": "G711U",
     },
+    "2025-01-10,10:00:04,004,00010": {
+        "gw_number": "004",
+        "session_id": "00021",
+        "start_time": "2025-01-10,10:00:04",
+        "end_time": "10:10:04",
+        "local_addr": "10.10.48.4",
+        "local_port": "2064",
+        "remote_addr": "192.168.148.195",
+        "remote_port": "2052",
+        "qos":  "Faulted",
+        "codec": "G711U",
+    },
+    "2025-01-10,10:00:05,005,00020": {
+        "gw_number": "005",
+        "session_id": "00022",
+        "start_time": "2025-01-10,10:00:05",
+        "end_time": "10:10:05",
+        "local_addr": "10.10.48.5",
+        "local_port": "2064",
+        "remote_addr": "10.10.48.63",
+        "remote_port": "2054",
+        "qos":  "Faulted",
+        "codec": "G711U",
+    },
+    "2025-01-10,10:00:06,006,00020": {
+        "gw_number": "006",
+        "session_id": "00041",
+        "start_time": "2025-01-10,10:00:06",
+        "end_time": "10:10:06",
+        "local_addr": "10.10.48.6",
+        "local_port": "2064",
+        "remote_addr": "10.10.48.55",
+        "remote_port": "34442",
+        "qos":  "Faulted",
+        "codec": "G711U",
+    },
 })
 
-row_iterator = list(storage.values())
+row_iterator = lambda start,stop: islice(storage.values(), start, stop)
 col_iterator = lambda c: [
     c.get('start_time'), c.get('end_time'), c.get('gw_number'),
     c.get('local_addr'), c.get('local_port'), c.get('remote_addr'),
     c.get('remote_port'), c.get('codec'), c.get('qos')]
-
 
 
 class Workspace():
@@ -81,9 +117,11 @@ class Workspace():
         self._menuwin = menu_window
         self._yoffset = yoffset
         self._xoffset = xoffset
-        self._row_iterator = row_iterator
+        self._storage = storage
         self._col_iterator = col_iterator
-        self._cursy = 0
+        self.posy = 0
+        self.posx = 0
+        self.row_pos = 0
         self.color_pair = color_pair if color_pair else curses.color_pair(0)
         self.maxy = self._stdscr.getmaxyx()[0] - yoffset
         self.maxx = sum(x + 1 for x in self._col_widths) + 1
@@ -117,19 +155,40 @@ class Workspace():
         self.title.refresh()
 
     def _draw_body(self):
-        for ridx, row in enumerate(self._row_iterator[self._cursy:  self._cursy + self.maxy - 3]):
+        start_row = max(0, self.row_pos - (self.maxy - 4))
+        end_row = max(self.row_pos + 1, (self.maxy - 3))
+        for ridx, row in enumerate(self._storage[start_row, end_row]):
             offset = 0
-            for cidx, (item, width) in enumerate(zip(self._col_iterator(row), self._col_widths)):
-                xpos = self._xoffset if cidx == 0 else offset
-                item = f"│{item[-width:]:>{width}}"
-                offset = xpos + len(item)
-                self.body.addstr(ridx, xpos, item, self.color_pair)
-            self.body.addstr(ridx, xpos + len(item), "│", self.color_pair)
-            self.body.refresh()
+            try:
+                for cidx, (item, width) in enumerate(zip(self._col_iterator(row), self._col_widths)):
+                    xpos = self._xoffset if cidx == 0 else offset
+                    item = f"│{item[-width:]:>{width}}"
+                    offset = xpos + len(item)
+                    cpair = self.color_pair|curses.A_REVERSE if ridx == self.posy else self.color_pair
+                    self.body.addstr(ridx, xpos, item, cpair)
+                self.body.addstr(ridx, xpos + len(item), "│", cpair)
+            except curses.error:
+                pass
+        self.body.refresh()
 
     def _draw_footer(self):
-        self.footer.addstr(0, 0, " " * (self.maxx - 1), self.color_pair|curses.A_REVERSE)
+        #self.footer.addstr(0, 0, " " * (self.maxx - 1), self.color_pair|curses.A_REVERSE)
         self.footer.refresh()
+
+    def handle_char(self, char):
+        if char == curses.KEY_DOWN:
+            self.posy = self.posy + 1 if self.posy < self.maxy - 4 else self.posy
+            self.row_pos = self.row_pos + 1 if len(self._storage) > self.row_pos + 1 else self.row_pos
+        elif char == curses.KEY_UP:
+            self.posy =  self.posy - 1 if self.posy > 0 else self.posy
+            self.row_pos = self.row_pos - 1 if self.row_pos > 0 else self.row_pos
+        elif char == curses.KEY_HOME:
+            self.posy = 0
+            self.row_pos = 0
+        elif char == curses.KEY_END:
+            self.posy = self.maxy - 4
+            self.row_pos = len(self._storage) - 1
+        self._draw_body()
 
 def main(stdscr):
     curses.curs_set(0)
@@ -138,10 +197,12 @@ def main(stdscr):
     curses.use_default_colors()
     stdscr.nodelay(True)
     done = False
+    workspaces = []
+    active_ws_idx = 0    
 
     while not done:
     
-        ws = Workspace(
+        ws1 = Workspace(
             stdscr,
             col_attrs=["start_time", "stop_time", "gw_number",
                 "local_address", "local_port", "remote_address",
@@ -152,6 +213,9 @@ def main(stdscr):
             row_iterator=row_iterator,
             col_iterator=col_iterator,
         )
+
+        workspaces.append(ws1)
+
         while not done:
             char = stdscr.getch()
             if char == curses.ERR:
@@ -159,6 +223,10 @@ def main(stdscr):
             elif char == curses.KEY_RESIZE:
                 stdscr.erase()
                 break
+            elif char in (curses.KEY_DOWN, curses.KEY_UP, curses.KEY_HOME, curses.KEY_END):
+                workspaces[active_ws_idx].handle_char(char)
+            elif chr(char) == "\t":
+                active_ws_idx = (active_ws_idx + 1) % len(workspaces)
             elif chr(char) == "q":
                 sys.exit()
 
